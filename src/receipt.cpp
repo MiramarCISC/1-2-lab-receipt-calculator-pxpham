aedfa6e424d615d1b48f3daa49358bd977434667
Elijah Jones <elijahpjones@outlook.com>
2026-06-20 03:54:20 +0000

    Peer Review

--- src/receipt.cpp
+++ src/receipt.cpp
@@ -1,23 +1,23 @@
 #include "../include/receipt.hpp"
 
-// TODO: Return the sum of all three item prices.
+// Returns the sum of all three item prices.
 double calculateSubtotal(double item1, double item2, double item3) {
     return item1 + item2 + item3;
 }
 
-// TODO: Return the sales tax for the subtotal using TAX_RATE.
+// Returns the sales tax for the subtotal using TAX_RATE.
 double calculateTax(double subtotal) {
     return subtotal * TAX_RATE;
 }
 
-// TODO: Return the tip amount.
+// Returns the tip amount.
 // Example: if subtotal is 20.00 and tipPercent is 15,
 // the tip should be 3.00.
 double calculateTip(double subtotal, double tipPercent) {
     return subtotal * (tipPercent/100);
 }
 
-// TODO: Return the final total.
+// Returns the final total.
 double calculateTotal(double subtotal, double tax, double tip) {
     return subtotal + tax + tip;
 }
